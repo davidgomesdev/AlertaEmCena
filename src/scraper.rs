5330e22547c30dc092154552ae6d4cd4c44d8f69
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2024-06-30 12:18:26 +0000

    feat: scrape thumbnail and only of presential pieces

--- src/scraper.rs
+++ src/scraper.rs
@@ -2,7 +2,7 @@ use std::error::Error;
 
 use lazy_static::lazy_static;
 
-use scraper::{Html, Selector};
+use scraper::{ElementRef, Html, Selector};
 
 const BOL_BASE_URL: &str = "https://www.bol.pt";
 
@@ -11,46 +11,66 @@ lazy_static! {
         "{}/Comprar/pesquisa/1-101-0-0-0-0/bilhetes_de_teatro_arte_teatro",
         BOL_BASE_URL
     );
+    static ref PIECE_ITEMS_SELECTOR: Selector = Selector::parse(".itens").unwrap();
+    static ref PIECE_NAME_SELECTOR: Selector =
+        Selector::parse(".item-montra.evento > .overlay > .nome").unwrap();
+    static ref PIECE_IMAGE_SELECTOR: Selector = Selector::parse("#ImagemEvento").unwrap();
 }
-
-const PIECES_SELECTOR: &str = ".item-montra > .overlay > .nome";
 
 #[derive(Debug, Clone)]
 pub struct Piece {
     pub name: String,
     pub url: String,
+    pub thumbnail_url: String,
 }
 
 impl Piece {
-    pub fn new(name: &str, url: &str) -> Piece {
+    pub fn new(name: &str, piece_path: &str, thumbnail_url: &str) -> Piece {
         Piece {
             name: name.to_string(),
-            url: format!("{}{}", BOL_BASE_URL.to_owned(), url.to_string()),
+            url: format!("{}{}", BOL_BASE_URL.to_owned(), piece_path),
+            thumbnail_url: thumbnail_url.to_string(),
         }
     }
 }
 
 pub async fn scrape_bol() -> Result<Vec<Piece>, Box<dyn Error>> {
-    let html = crawl_bol_pieces_page().await?;
-    let fragment = Html::parse_fragment(&html);
-    let selector = Selector::parse(PIECES_SELECTOR)?;
+    let html = crawl_page(PIECES_URL.to_owned()).await?;
 
-    let piece_titles = fragment
-        .select(&selector)
-        .map(|el| {
-            Piece::new(
-                &el.inner_html().replace("\"", ""),
-                el.attr("href")
-                    .expect(&format!("Got invalid link on {}", el.html())),
-            )
-        })
-        .collect::<Vec<Piece>>();
+    let piece_titles = futures::future::join_all(html
+        .select(&PIECE_ITEMS_SELECTOR).next().unwrap()
+        .select(&PIECE_NAME_SELECTOR)
+        .map(|el| scrape_piece(el)))
+        .await;
 
     Ok(piece_titles)
 }
 
-async fn crawl_bol_pieces_page() -> Result<String, Box<dyn Error>> {
-    let page_html = reqwest::get(PIECES_URL.to_owned()).await?.text().await?;
+async fn scrape_piece(element: ElementRef<'_>) -> Piece {
+    let piece_path = element
+        .attr("href")
+        .unwrap_or_else(|| panic!("Got invalid link on {}", element.html()));
 
-    Ok(page_html)
+    let piece_html = crawl_page(format!("{}{}", BOL_BASE_URL.to_owned(), piece_path))
+                .await
+                .expect("fix");
+
+    let thumbnail_url = piece_html
+        .select(&PIECE_IMAGE_SELECTOR)
+        .next()
+        .unwrap_or_else(|| panic!("No image found on {}", element.html()))
+        .attr("src")
+        .unwrap_or_else(|| panic!("No image URL found on {}", element.html()));
+
+    Piece::new(
+        &element.inner_html().replace('"', ""),
+        piece_path,
+        thumbnail_url,
+    )
+}
+
+async fn crawl_page(url: String) -> Result<Html, Box<dyn Error>> {
+    let page_html = reqwest::get(url).await?.text().await?;
+
+    Ok(Html::parse_fragment(&page_html))
 }
