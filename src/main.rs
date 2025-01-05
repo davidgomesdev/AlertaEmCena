a52589d934e7932c42f5f96c41f38c98968edb80
David Gomes <david.gomes@daimlertruck.com>
2025-01-05 11:45:18 +0000

    feat: add categories and sort asc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use crate::api::model::Category;
 use api::api::get_events;
 use tracing::info;
 
@@ -7,7 +8,7 @@ mod api;
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let events = get_events().await.unwrap();
+    let events = get_events(10000, &Category::Teatro).await.unwrap();
 
     events.iter().for_each(|event| info!("Got event {}", event.title));
 }
