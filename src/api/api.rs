8f989214df113ff29f21d6a041f01d466b31a32b
David Gomes <david.gomes@daimlertruck.com>
2025-01-05 11:45:18 +0000

    feat: add categories and sort asc

--- src/api/api.rs
+++ src/api/api.rs
@@ -1,20 +1,33 @@
-use tracing::error;
-
 use super::{dto::ResponseEvent, model::Event};
+use crate::api::model::Category;
+use tracing::error;
 
 const AGENDA_EVENTS_URL: &str = "https://www.agendalx.pt/wp-json/agendalx/v1/events";
 
-pub async fn get_events() -> Result<Vec<Event>, APIError> {
-    let json_response = reqwest::get(format!("{}?search=&per_page=10&categories=teatro&type=event", AGENDA_EVENTS_URL))
-        .await.unwrap().text().await.unwrap();
+/*
+   Returns events with ascending order
+*/
+pub async fn get_events(amount_per_page: i32, category: &Category) -> Result<Vec<Event>, APIError> {
+    let category: &'static str = category.into();
+    let json_response = reqwest::get(format!(
+        "{}?per_page={}&categories={}&type=event",
+        AGENDA_EVENTS_URL,
+        amount_per_page,
+        category.to_lowercase()
+    ))
+    .await
+    .unwrap()
+    .text()
+    .await
+    .unwrap();
     let parsed_response = serde_json::from_str::<Vec<ResponseEvent>>(&json_response);
 
-    return parsed_response
-        .map(|mut res| res.iter_mut().map(|e| e.to_model()).collect())
+    parsed_response
+        .map(|mut res| res.iter_mut().map(|e| e.to_model()).rev().collect())
         .map_err(|e| {
             error!("API response cannot be parsed! {}", e);
             APIError::InvalidResponse
-        });
+        })
 }
 
 #[derive(Debug)]
