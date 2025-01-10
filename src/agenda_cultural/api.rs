44bfe384f81c538995f8277400e4db1cb70113ef
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2025-01-10 18:58:22 +0000

    feat: add retries

--- src/agenda_cultural/api.rs
+++ src/agenda_cultural/api.rs
@@ -1,27 +1,43 @@
 use super::{dto::ResponseEvent, model::Event};
 use crate::agenda_cultural::model::Category;
 use futures::future;
+use lazy_static::lazy_static;
+use reqwest::Client;
+use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
+use reqwest_retry::policies::ExponentialBackoff;
+use reqwest_retry::RetryTransientMiddleware;
 
 const AGENDA_EVENTS_URL: &str = "https://www.agendalx.pt/wp-json/agendalx/v1/events";
 const EVENT_TYPE: &str = "event";
+const MAX_RETRIES: u32 = 5;
+
+lazy_static! {
+    static ref REST_CLIENT: ClientWithMiddleware = ClientBuilder::new(Client::new())
+        .with(RetryTransientMiddleware::new_with_policy(
+            ExponentialBackoff::builder().build_with_max_retries(MAX_RETRIES)
+        ))
+        .build();
+}
 
 /**
    Returns events with ascending order
 */
 pub async fn get_events(amount_per_page: i32, category: &Category) -> Result<Vec<Event>, APIError> {
     let category: &'static str = category.into();
-    let json_response = reqwest::get(format!(
-        "{}?per_page={}&categories={}&type={}",
-        AGENDA_EVENTS_URL,
-        amount_per_page,
-        category.to_lowercase(),
-        EVENT_TYPE
-    ))
-    .await
-    .unwrap()
-    .text()
-    .await
-    .unwrap();
+    let json_response = REST_CLIENT
+        .get(format!(
+            "{}?per_page={}&categories={}&type={}",
+            AGENDA_EVENTS_URL,
+            amount_per_page,
+            category.to_lowercase(),
+            EVENT_TYPE
+        ))
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
     let parsed_response = serde_json::from_str::<Vec<ResponseEvent>>(&json_response);
 
     match parsed_response {
