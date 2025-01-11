38c0903041789f2caa922ad85f34c3b24bb83848
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2025-01-11 14:44:14 +0000

    feat: get events already sent

--- tests/discord_api_tests.rs
+++ tests/discord_api_tests.rs
@@ -1,19 +1,21 @@
 use alertaemcena::discord::api::DiscordAPI;
 use lazy_static::lazy_static;
 use std::env;
+use serenity::all::ChannelId;
+use uuid::Uuid;
 use alertaemcena::agenda_cultural::model::*;
 
 lazy_static! {
     static ref token: String = env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN not set");
-    static ref channelId: String =
-        env::var("DISCORD_CHANNEL_ID").expect("DISCORD_CHANNEL_ID not set");
+    static ref channel_id: ChannelId =
+        env::var("DISCORD_CHANNEL_ID").expect("DISCORD_CHANNEL_ID not set").parse().unwrap();
 }
 
 #[tokio::test]
-async fn test_discord_api() {
+async fn should_send_event() {
     build_api()
         .await
-        .send_event(channelId.parse().unwrap(), Event {
+        .send_event(*channel_id, Event {
             title: "O Auto da Barca do Inferno".to_string(),
             details: EventDetails {
                 subtitle: "Uma peça de Gil Vicente".to_string(),
@@ -28,6 +30,48 @@ async fn test_discord_api() {
             venue: "Teatro Nacional D. Maria II, Lisboa".to_string(),
         })
         .await;
+}
+
+#[tokio::test]
+async fn should_read_events() {
+    build_api()
+        .await
+        .get_event_urls_sent(*channel_id)
+        .await;
+}
+
+#[tokio::test]
+async fn should_read_event_sent() {
+    let test_id = Uuid::new_v4();
+    let link = format!("https://example.com/events/barca_infern?test_id={}", test_id);
+    let unique_event = Event {
+        title: "O Auto da Barca do Inferno".to_string(),
+        details: EventDetails {
+            subtitle: "Uma peça de Gil Vicente".to_string(),
+            description: "Uma comédia dramática que reflete sobre os vícios humanos e as escolhas morais.".to_string(),
+            image_url: "https://www.culturalkids.pt/wp-content/uploads/2021/04/Auto-01_1.jpg".to_string(),
+        },
+        link: link.to_string(),
+        occurring_at: Schedule {
+            dates: "21 setembro 2024 a 23 fevereiro 2025".to_string(),
+            times: "qui: 21h; sex: 21h; sáb: 21h; dom: 17h".to_string(),
+        },
+        venue: "Teatro Nacional D. Maria II, Lisboa".to_string(),
+    };
+
+    let api = build_api()
+        .await;
+
+    api
+        .send_event(*channel_id, unique_event)
+        .await;
+
+    let is_event_sent = api
+        .get_event_urls_sent(*channel_id)
+        .await
+        .contains(&link);
+
+    assert!(is_event_sent);
 }
 
 async fn build_api() -> DiscordAPI {
