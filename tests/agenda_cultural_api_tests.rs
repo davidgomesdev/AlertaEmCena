0cef2f90b784e86c2f25b8e2f873c6a52b4f3f31
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2025-01-18 22:01:01 +0000

    feat: scrape all events

--- tests/agenda_cultural_api_tests.rs
+++ tests/agenda_cultural_api_tests.rs
@@ -4,7 +4,7 @@ use alertaemcena::agenda_cultural::model::Category;
 #[tokio::test]
 #[test_log::test]
 async fn should_scrape_teatro_events() {
-  let res = AgendaCulturalAPI::get_events(5, &Category::Teatro)
+  let res = AgendaCulturalAPI::get_events(&Category::Teatro, Some(5))
       .await
       .unwrap();
 
@@ -14,7 +14,7 @@ async fn should_scrape_teatro_events() {
 #[tokio::test]
 #[test_log::test]
 async fn should_scrape_artes_events() {
-  let res = AgendaCulturalAPI::get_events(2, &Category::Artes)
+  let res = AgendaCulturalAPI::get_events(&Category::Artes, Some(2))
       .await
       .unwrap();
 
