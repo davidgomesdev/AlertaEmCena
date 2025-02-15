294c79f8db1115c2d8e11a1fbc931fdfe8a585e8
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2025-02-15 12:49:25 +0000

    feat: add save later reaction to existing events

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::process::exit;
 use alertaemcena::agenda_cultural::api::AgendaCulturalAPI;
 use alertaemcena::agenda_cultural::model::{Category, Event};
 use alertaemcena::config::env_loader::load_config;
-use alertaemcena::config::model::{DebugConfig, EmojiConfig};
+use alertaemcena::config::model::{Config, DebugConfig, EmojiConfig};
 use alertaemcena::discord::api::DiscordAPI;
 use serenity::all::{ChannelId, Message};
 use tracing::{debug, info, instrument};
@@ -26,22 +26,27 @@ async fn main() {
         }
     }
 
+    run(&config, &discord, Category::Artes, config.artes_channel_id).await;
+    run(&config, &discord, Category::Teatro, config.teatro_channel_id).await;
+}
+
+async fn run(config: &Config, discord: &DiscordAPI, category: Category, channel_id: ChannelId) {
+    backfill_save_later_reaction(discord, channel_id).await;
+
     send_new_events(
         &discord,
-        &Category::Teatro,
-        config.teatro_channel_id,
+        &category,
+        channel_id,
         &config.debug_config,
         &config.voting_emojis,
     )
-    .await;
-    send_new_events(
-        &discord,
-        &Category::Artes,
-        config.artes_channel_id,
-        &config.debug_config,
-        &config.voting_emojis,
-    )
-    .await;
+        .await;
+}
+
+async fn backfill_save_later_reaction(discord: &DiscordAPI, channel_id: ChannelId) {
+    info!("Backfilling save later reaction");
+
+    discord.add_reaction_to_all_messages(channel_id, '🔖').await;
 }
 
 #[instrument(skip(discord, channel_id, emojis), fields(channel_id = %channel_id.to_string()))]
@@ -52,6 +57,8 @@ async fn send_new_events(
     debug_config: &DebugConfig,
     emojis: &[EmojiConfig; 5],
 ) {
+    info!("Sending new events");
+
     let new_events = get_new_events(discord, category, channel_id, debug_config.event_limit).await;
 
     if new_events.is_empty() {
