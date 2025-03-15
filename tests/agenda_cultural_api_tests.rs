380d4d1b4c5894308bc335f4f99a4121de6fbbf8
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2025-03-15 16:15:57 +0000

    feat: add api to get an event by public url and id

--- tests/agenda_cultural_api_tests.rs
+++ tests/agenda_cultural_api_tests.rs
@@ -1,5 +1,5 @@
 use alertaemcena::agenda_cultural::api::AgendaCulturalAPI;
-use alertaemcena::agenda_cultural::model::Category;
+use alertaemcena::agenda_cultural::model::{Category, Event};
 
 #[test_log::test(tokio::test)]
 async fn should_scrape_teatro_events() {
@@ -17,4 +17,38 @@ async fn should_scrape_artes_events() {
         .unwrap();
 
     assert_eq!(res.len(), 2);
+}
+
+#[test_log::test(tokio::test)]
+async fn should_scrape_the_specified_event_by_public_url() {
+    let event: Event = AgendaCulturalAPI::get_event_by_public_url("https://www.agendalx.pt/events/event/nora-helmer/")
+        .await
+        .unwrap();
+
+    assert_eq!(event.title, "Nora Helmer");
+    assert_eq!(event.details.description, "A história de Nora Helmer, protagonista de Casa de Bonecas, peça de Henrik Ibsen, torna-se o ponto de partida para um debate aceso sobre a família, o casamento e o lugar da mulher na sociedade. Ler mais.");
+    assert_eq!(event.details.image_url, "https://www.agendalx.pt/content/uploads/2025/02/Nora-Helmer_ensaios2©Filipe_Figueiredo.jpg");
+    assert_eq!(event.details.subtitle, "A partir de Henrik Ibsen e Lucas Hnath");
+    assert_eq!(event.link, "https://www.agendalx.pt/events/event/nora-helmer/");
+    assert_eq!(event.occurring_at.dates, "8 março a 20 abril");
+    assert_eq!(event.occurring_at.times, "qua: 19h; qui: 19h; sex: 21h; sáb: 21h; dom: 16h");
+    assert_eq!(event.venue, "Teatro Aberto");
+    assert_eq!(event.tags.is_empty(), true);
+}
+
+#[test_log::test(tokio::test)]
+async fn should_scrape_the_specified_event_by_id() {
+    let event: Event = AgendaCulturalAPI::get_event_by_id(208058)
+        .await
+        .unwrap();
+
+    assert_eq!(event.title, "Nora Helmer");
+    assert_eq!(event.details.description, "A história de Nora Helmer, protagonista de Casa de Bonecas, peça de Henrik Ibsen, torna-se o ponto de partida para um debate aceso sobre a família, o casamento e o lugar da mulher na sociedade. Ler mais.");
+    assert_eq!(event.details.image_url, "https://www.agendalx.pt/content/uploads/2025/02/Nora-Helmer_ensaios2©Filipe_Figueiredo.jpg");
+    assert_eq!(event.details.subtitle, "A partir de Henrik Ibsen e Lucas Hnath");
+    assert_eq!(event.link, "https://www.agendalx.pt/events/event/nora-helmer/");
+    assert_eq!(event.occurring_at.dates, "8 março a 20 abril");
+    assert_eq!(event.occurring_at.times, "qua: 19h; qui: 19h; sex: 21h; sáb: 21h; dom: 16h");
+    assert_eq!(event.venue, "Teatro Aberto");
+    assert_eq!(event.tags.is_empty(), true);
 }
