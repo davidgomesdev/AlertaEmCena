0afd788184bb0468f126111f70802d05f1bd59c6
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2025-06-06 15:27:50 +0100

    feat: add for children label

--- tests/agenda_cultural_api_tests.rs
+++ tests/agenda_cultural_api_tests.rs
@@ -74,6 +74,7 @@ mod agenda_cultural {
             event.occurring_at.times,
             "qua: 19h; qui: 19h; sex: 21h; sáb: 21h; dom: 16h"
         );
+        assert!(!event.is_for_children);
         assert_eq!(event.venue, "Teatro Aberto");
         assert!(event.tags.is_empty());
     }
@@ -83,8 +84,8 @@ mod agenda_cultural {
         let event: Event = AgendaCulturalAPI::get_event_by_public_url(
             "https://www.agendalx.pt/events/event/king-size-2/",
         )
-        .await
-        .unwrap();
+            .await
+            .unwrap();
 
         assert_eq!(event.title, "King Size");
         assert_eq!(event.details.description, "Através do burlesco dos shows drag king, Sónia Baptista, acompanhada pelas performers Ana Libório, Crista Alfaiate e Joana Levi, desconstrói sem apelo nem agravo, mas com um irresistível humor, os códigos de construção da masculinidade. +");
@@ -104,9 +105,43 @@ mod agenda_cultural {
         );
         assert_eq!(event.venue, "Sala Estúdio Valentim de Barros");
         assert_eq!(event.tags.len(), 4);
+        assert!(!event.is_for_children);
         assert_eq!(
             event.tags,
             ["+16", "performance", "queer", "Sónia Baptista"]
         );
+    }
+
+    #[test_log::test(tokio::test)]
+    async fn should_scrape_and_classify_a_children_piece_as_such() {
+        let event: Event = AgendaCulturalAPI::get_event_by_public_url(
+            "https://www.agendalx.pt/events/event/um-sapato-especial/",
+        )
+            .await
+            .unwrap();
+
+        assert_eq!(event.title, "Um sapato especial");
+        assert_eq!(event.details.description, "O Ursinho José gosta muito de ir brincar para o jardim. Joga à bola, às corridas, anda de bicicleta e nos baloiços. E ele é o campeão dos saltos! Mas um dia acontece algo inesperado e começa uma aventura que lhe trará novos amigos. Com o bombeiro Mário, a Dra. Malaquias e a enfermeira Juju, o Ursinho José vai aprender a divertir-se em segurança.");
+        assert_eq!(
+            event.details.image_url,
+            "https://www.agendalx.pt/content/uploads/2018/09/T-SE-cartaz1.jpg"
+        );
+        assert_eq!(event.details.subtitle, "");
+        assert_eq!(
+            event.link,
+            "https://www.agendalx.pt/events/event/um-sapato-especial/"
+        );
+        assert_eq!(event.occurring_at.dates, "14 junho");
+        assert_eq!(
+            event.occurring_at.times,
+            "16h00"
+        );
+        assert_eq!(event.venue, "Fábrica Braço de Prata");
+        assert_eq!(event.tags.len(), 2);
+        assert_eq!(
+            event.tags,
+            ["crianças", "famílias"]
+        );
+        assert!(event.is_for_children);
     }
 }
